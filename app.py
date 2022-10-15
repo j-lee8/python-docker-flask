10fdd55e24ce5bd814ea897c3d14322f23d9f63f
Jamie <jamie@Jamies-MacBook-Air.local>
2022-10-15 12:47:52 +0100

    Updated to show basic page linking to page which displays launch json.

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import requests
-import json
-import pandas as pd
+# import pandas as pd
 from flask import Flask, render_template
 app = Flask(__name__)
 
@@ -15,12 +14,17 @@ query = """query {
 }"""
 
 @app.route("/")
-def get_launches():
+def index():
+    return render_template('index.html')
+
+@app.route("/ships")
+def get_ships():
     url = 'https://api.spacex.land/graphql/'
     r = requests.post(url, json={'query': query})
     print(r.status_code)
     print(r.text)
-    return render_template('index.html', name=r.text)
+
+    return render_template('ships.html', data=r.text) 
 
 if __name__ == "__main__":
   app.run(debug=True)
